40dc69b06d1df91368935d8d5f7a37a375c4d247
sin-2-x <silantuys@mail.ru>
2022-04-02 12:41:20 +0300

    что-то с рабочим mvvm

---
+++ Lab4/Model/DebtorsModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.Model {
+  class DebtorsModel {
+    private List<string> repositoryData;
+    public string ImportantInfo {
+      get {
+        return ConcatenateData(repositoryData);
+      }
+    }
+
+    public DebtorsModel() {
+      repositoryData = GetData();
+    }
+
+    /// <summary>
+    /// Simulates data retrieval from a repository
+    /// </summary>
+    /// <returns>List of strings</returns>
+    private List<string> GetData() {
+      repositoryData = new List<string>()
+      {
+                "Hello",
+                "world"
+            };
+      return repositoryData;
+    }
+
+    /// <summary>
+    /// Concatenate the information from the list into a fully formed sentence.
+    /// </summary>
+    /// <returns>A string</returns>
+    private string ConcatenateData(List<string> dataList) {
+      string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+      return importantInfo;
+    }
+  }
+}
---
+++ Lab4/ViewModel/DebtorsViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Lab4.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.VeiwModel {
+  class DebtorsViewModel : INotifyPropertyChanged {
+    // Implements INotifyPropertyChanged interface to support bindings
+
+    private string helloString;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string HelloString {
+      get {
+        return helloString;
+      }
+      set {
+        helloString = value;
+        OnPropertyChanged();
+      }
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string name = null) {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    public DebtorsViewModel() {
+      var helloWorldModel = new DebtorsModel();
+      helloString = helloWorldModel.ImportantInfo;
+    }
+
+  }
+}
